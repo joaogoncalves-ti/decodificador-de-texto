4523a4e77242f96ae1e85d4ae8b03d2395cec641
joao-gs <pessoal.alves20@gmail.com>
2023-01-02 18:18:51 -0300

    correções do beta

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
-function cripto(){
+function criptografar(){
     var textoCampo = document.querySelector("#campoTexto");
     var texto = String(textoCampo.value);
 
     var resposta = texto.replace(/e/g, "enter").replace(/i/g,"imes").replace(/o/g,"ober").replace(/a/g,"ai").replace(/u/g,"ufat");
 
-    
 
-   if(campoResposta.innerHTML = resposta){
-        document.querySelector(".botao-cripto").value = "Criptografado"
-   }
+    if(campoResposta.innerHTML = resposta){
+        document.querySelector(".botao-criptografar").value = "Criptografado"
+    }
 
-   setInterval (function(){
-    document.querySelector(".botao-cripto").value = "Criptografar";
-   },3000);
+    setInterval (function(){
+    document.querySelector(".botao-criptografar").value = "Criptografar";
+    }, 3000);
 
 }
 
-function descripto(){
+function descriptografar(){
     var textoCampo= document.querySelector("#campoTexto");
     var texto= String(textoCampo.value);
 
     var resposta = texto.replace(/ai/g, "a").replace(/enter/g,"e").replace(/imes/g,"i").replace(/ober/g,"o").replace(/ufat/g,"u");
 
     if (campoResposta.innerHTML = resposta){
-        document.querySelector(".botao-descripto").value = "Descriptografado";
+        document.querySelector(".botao-descriptografar").value = "Descriptografado";
     }
 
     setInterval (function() {
-        document.querySelector(".botao-descripto").value = "Descriptografar";
-    },3000);
+        document.querySelector(".botao-descriptografar").value = "Descriptografar";
+    }, 3000);
 
 
 
@@ -39,15 +38,12 @@ function copiar(){
     var textoCopiado = document.querySelector("#campoResposta");
 
     if(navigator.clipboard.writeText(textoCopiado.value)){
-        document.getElementById("botaoCopiado").value = "Copiado";
+        document.getElementById("botaoCopiar").value = "Copiado";
     }
 
     setInterval (function() {
-        document.getElementById("botaoCopiado").value = "Copiar";
-    },3000);
+        document.getElementById("botaoCopiar").value = "Copiar";
+    }, 3000);
 
-   
-
-    
     
 }
